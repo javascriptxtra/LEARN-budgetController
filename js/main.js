427dacabe9cfa2f7796e40c66502b6bb3258edcc
javascriptxtra <javascriptxtra@gmail.com>
2019-03-18 07:05:30 -0400

    another yet commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 let controller = (function(budgetCtrl,UICtrl){
+    console.log("started");   
+    let DOM = UICtrl.getDOMstrings; 
+
+    let ctrlAddItem = function(){
+        // 1. Get the filed input data
+        let input = UICtrl.getInput();
+        console.log(input);
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calcualte the budget
+
+        // 5. Display the budget on the UI
     
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(e){
+        if (e.keyCode === 13 || e.which === 13 ) {
+            ctrlAddItem();
+        }
+    })
 
 })(data,ui);
 
--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,20 @@
 let ui = (function(){
-    
+   let DOMstrings = {
+       inputType: '.add__type',
+       inputDescription: '.add__description',
+       inputValue: '.add__value'
+   };
+
+   return {
+       getInput: function(){
+           return {
+               type: document.querySelector(DOMstrings.inputType),
+               description: document.querySelector(DOMstrings.inputDescription),
+               value: document.querySelector(DOMstrings.inputValue).value
+           }
+       },
+       getDOMstrings: function(){
+           return DOMstrings;
+       }
+   }
 })();
